3bbea2a78ada8679bc4c50c2fc27767506f148f0
AbdulquddusMuhammad <abdulquddusmuhammad472@gmail.com>
2026-02-10 11:29:10 +0100

    fixed font size

--- src/components/EventItemShowCase.jsx
+++ src/components/EventItemShowCase.jsx
@@ -46,16 +46,16 @@ const EventItemShowCase = () => {
               <div onClick={() => handleViewImage(product.image)} key={index} className="eventProductCard shadow-lg rounded-lg w-[45vw] md:w-[21vw] px-3 mb-6">
                 <img src={product.image} alt={product.name} className="eventProductImage w-full h-[30vh] md:h-[40vh]" />
                 <div className="flex justify-between p-2 md:p-5">
-                  <div className="text-[2.5vw]" style={{ fontFamily: "Playfair Display" }}>
+                  <div className="text-[2.5vw] md:text-[1.2vw]" style={{ fontFamily: "Playfair Display" }}>
                     {product.name}
                   </div>
-                  <div style={{ fontFamily: "Manrope" }} className="text-[2.5vw]">
+                  <div style={{ fontFamily: "Manrope" }} className="text-[2.5vw] md:text-[1.2vw]">
                     {product.no_ofImages} Images
                   </div>
                 </div>
                 <div className="flex pb-1.5 md:pb-5 px-2 md:px-5 items-center">
                   <img src={calenderIcon} alt="Calendar Icon" className="w-[3vw] md:w-5 md:h-5" />
-                  <div className="ml-2 text-[#00000080] text-[2vw]" style={{ fontFamily: "Manrope" }}>
+                  <div className="ml-2 text-[#00000080] text-[2vw] md:text-[1.2vw]" style={{ fontFamily: "Manrope" }}>
                     {product.date}
                   </div>
                 </div>
